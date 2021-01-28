8457c186ce89a3a2977e5acece0a45a6311f0d60
Manish Kumar <manishkumar2195@gmail.com>
2021-01-28 14:22:10 +0530

    category schema updated

--- api/models/category.js
+++ api/models/category.js
@@ -5,7 +5,11 @@ const categorySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     // name: famous cities, famous attraction places, adventurous experiences, places you may visit at least once, favourable and economical tours etc.
     name: { type: String, required: true, unique: true, index: true },
-    place: { type: [mongoose.Mongoose.type.ObjectId] },
+    place: {
+        id: { type: [mongoose.Mongoose.type.ObjectId] },
+        name: { type: String },
+        thumbnail: { type: String },
+    },
     placetype: { type: String, index: true },    // eg. place/city/country etc. name of the collection
 });
 
